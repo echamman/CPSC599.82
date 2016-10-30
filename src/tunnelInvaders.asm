e5e87621fed33d5bd98abc1339b2acfbb2425067
Ethan Hamman <e.hamman@telus.net>
2016-10-30 13:27:17 -0600

    Restructuring

---
+++ src/tunnelInvaders.asm
@@ -0,0 +1,25 @@
+;The full game
+
+
+	processor 6502
+
+	seg
+	org $1001
+
+	;This is the stub, it calls SYS 4400, 4400 being the arbitrary address we are writing our machine code at
+	stub	.BYTE #$0	;New line
+	.BYTE #$0C	;arbitary link values (Non-zero)
+	.BYTE #$0A
+	.BYTE #$0	;new line
+	.BYTE #$9E	;SYS
+	.BYTE #$20	;space
+	.BYTE #$34	;4
+	.BYTE #$34	;4
+	.BYTE #$30	;0
+	.BYTE #$30	;0
+	.BYTE #$0	;END CODE
+	.BYTE #$0
+	.BYTE #$0
+
+	seg code
+	org $1130	;Address 4400
