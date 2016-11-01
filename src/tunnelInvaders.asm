95366dab5d3c0b28471fcb8a23a0f0f75f5cc6b6
Buttonz52 <Brendan_Petras@hotmail.com>
2016-11-01 14:24:27 -0600

    Added ship Data & printed it in Main

--- src/tunnelInvaders.asm
+++ src/tunnelInvaders.asm
@@ -23,8 +23,9 @@ stub	.BYTE #$0	;New line
 	seg code
 	org $100E	;Address 4110, right after stub
 
+    JSR setchars     ;Loads charset from ROM to RAM
     JSR intro
-gameloop
+gameloop            ;check input,update data, draw data to screen
     JSR main
     LDA #$01
     BNE gameloop
@@ -71,14 +72,15 @@ msg			        ;Prints 'PRESS START' - Appendix E
 	LDA #$12
 	STA $1EBF
 	LDA #$14
-	STA $1EC0
+	STA $1EC0   
 
 	LDA #$00		;port input mask
 	STA $9113		;store to VIA#1 DDR
 quitIntro
 	LDA $9111		;load joystick input
 	EOR #$DF		;XOR against fire button bitmask
-	BNE intro		;branch up on no input
+	BNE quitIntro		;branch up on no input
+    JSR clearscreen
     RTS
 
 clearscreen
@@ -108,66 +110,115 @@ psf
 	LDA $9111		;load joystick input
 	EOR #$DF		;XOR against bitmask
 	BNE psu			;branch to next check
-	STY #$01		;1 is stored to Y if fire is held down
+	LDY #$01		;1 is stored to Y if fire is held down
 	BEQ endInput
 psu
 	LDA $9111		;load joystick input
 	EOR #$FB		;XOR against bitmask
 	BNE psd			;branch to next check
-	STY #$02		;2 is stored to Y if up is held down
+	LDY #$02		;2 is stored to Y if up is held down
 	BEQ endInput
 psd
 	LDA $9111		;load joystick input
 	EOR #$F7		;XOR against bitmask
-	BNU psl			;branch to next check
-	STY #$03		;3 is stored to Y if down is held down
+	BNE psl			;branch to next check
+	LDY #$03		;3 is stored to Y if down is held down
 	BEQ endInput
 psl
 	LDA $9111		;load joystick input
 	EOR #$EF		;XOR against bitmask
 	BEQ psr			;branch to next check
-	STY #$04		;4 is stored to Y if left is held down
+	LDY #$04		;4 is stored to Y if left is held down
 	BEQ endInput
 psr
 	LDA $9120		;load joystick input (VIA2)
 	EOR #$7F		;XOR against bitmask
 	BEQ noPush		;branch to next check
-	STY #$05		;5 is stored to Y if right is held down
+	LDY #$05		;5 is stored to Y if right is held down
 	BEQ endInput
 noPush
-	STY #$00		;0 is stored to Y if nothing is pushed
+	LDY #$00		;0 is stored to Y if nothing is pushed
 endInput
 	STX $9122		;else restore VIA#2
 	RTS
 
-setchar             ;Store the character set in RAM
-                    ;replicate POKE 36859,255 = 8FFB,FF
-                    ;for i = 7168 to 7679:Poke I, PEEk(i+25600):NEXT
-                    
+setchars             ;Store the character set in RAM                            
+    LDA #$FF        ;Tell vic to read chars from RAM
+    STA $9005       ;poke 36869,255               
+    LDA #$1C        ;securing charset location
+    STA $0034       ;poke 52,28
+    LDA #$1C        ;securing charset location
+    STA $0038       ;poke 56,28: CLR
+  
     LDX #$00        ; load x=255 for for loop; loop from 0-511    
-loop1
-    LDA #$1C00+x,$memoffset
-    STA #$1C00,x
+setchar1
+    LDA #$8000,x   ;load each char from ROM
+    ;LDA #$FF        ;make each char a block
+    STA #$1C00,x    ;store them in 7168-7423
     INX
     CPX #$FF
-    BNE loop1
+    BNE setchar1
     
+    LDX #$00
+setchar2
+    LDA #$8100,x
+    ;LDA #$FF
+    STA #$1D00,x    ;store them in 7424-7680
+    INX
+    CPX #$FF
+    BNE setchar2
+    
+storeship          ;function that draws the screen based on whats stored
+    LDX #$0  
+storeship1    
+    LDA ship,x      ;Chatset location 27 or dec 7195-7267. 9 char change
+    STA $1D10,x
+    INX
+    CPX #$48       ;dec 72 = 9*8
+    BNE storeship1
     RTS
-
     
-main
-    JSR clearscreen
+main                ;this just draws our ship
+    LDA #$22 
+    STA $1E00
+    LDA #$23
+    STA $1E01
+    LDA #$24 
+    STA $1E02
+    LDA #$25 
+    STA $1E16
+    LDA #$26 
+    STA $1E17
+    LDA #$27 
+    STA $1E18  
+    LDA #$28 
+    STA $1E2C
+    LDA #$29 
+    STA $1E2D
+    LDA #$2A 
+    STA $1E2E 
     JSR checkInput
     RTS
     ;memory the spaceship
     ;print it to screen.
 
-    
+;=============================================================================
+;notes
+;    LDA #$00 <- @
+;    STA $1E00 <- this puts accumulator in the write buffer    
     
 ;=============================================================================
 ;DATA
-    org 1800        ;dec  6144
+    org $1800        ;dec  6144
+             
+ship    
+    .BYTE   $00,$00,$00,$01,$07,$0F,$1F,$3F ;[0][0]
+    .BYTE   $00,$00,$00,$F8,$FE,$FF,$FF,$FF ;[0][1]
+    .BYTE   $00,$00,$00,$00,$00,$00,$80,$C0 ;[0][2]
+    .BYTE   $3F,$7F,$7F,$7F,$7F,$7F,$7F,$7F ;[1][0]
+    .BYTE   $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF ;[1][1]
+    .BYTE   $E0,$F8,$FC,$FF,$C0,$C0,$C0,$FF ;[1][2]
+    .BYTE   $7F,$3F,$3F,$1F,$0F,$07,$01,$00 ;[2][0]
+    .BYTE   $FF,$FF,$FF,$FF,$FF,$FF,$FB,$01 ;[2][1]
+    .BYTE   $FC,$F8,$E0,$C0,$E0,$F0,$F0,$E0 ;[2][2]
     
-memoffset   .BYTE $64,$00    ;dec 25600 const offset for charset mapping to RAM
-bgncharset  .BYTE $1C,$00                            
-
