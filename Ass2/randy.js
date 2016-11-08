ef99e50506032199c97202d1fa99f64c8a859420
Ethan Hamman <e.hamman@telus.net>
2016-11-08 12:51:45 -0700

    Random ai

---
+++ Ass2/randy.js
@@ -0,0 +1,13 @@
+function bertieMove(board){
+	var complete = false;
+	while(!complete){
+		var row = Math.floor(Math.random() * 2);
+		var col = Math.floor(Math.random() * 2)
+
+		if(board[row][col] == ' '){
+			board[row][col] = 'x';
+			complete = true;
+		}
+	}
+	return board;
+}
